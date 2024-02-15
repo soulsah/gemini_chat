7a2ee43a5106107f17a88f141adfcb51aac9f87c
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2024-02-15 13:23:36 -0300

    add conversation features

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { UserRound, Bot } from 'lucide-react';
 import axios from 'axios';
 import './App.css';
 
 type MessageType = {
   text: string;
-  type: 'user' | 'bot';
+  type: 'user' | 'model';
 };
 
 const Chatbot = () => {
   const [input, setInput] = useState<string>('');
   const [messages, setMessages] = useState<MessageType[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Mock da mensagem inicial do bot ao montar o componente
+    const initialBotMessage: MessageType = {
+      text:
+        'Olá! Agradecemos por entrar em contato com o serviço de consignado do grupo AMP. Podemos te ajudar com isso. Para começar, informe seu nome para darmos início ao atendimento',
+      type: 'model',
+    };
+
+    setMessages([initialBotMessage]);
+  }, []);
+
+  useEffect(() => {
+    // Scroll automático quando novas mensagens são adicionadas
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
 
   const sendMessage = async () => {
     if (input.trim() === '') return;
@@ -23,35 +43,56 @@ const Chatbot = () => {
         message: input,
       });
 
-      const botMessage: MessageType = { text: response.data.message, type: 'bot' };
+      const botMessage: MessageType = { text: response.data.message, type: 'model' };
       setMessages([...newMessages, botMessage]);
     } catch (error) {
       console.error('Erro ao enviar mensagem para o chatbot', error);
     }
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
   return (
     <div className="container">
-      <div className="header">Gemini Chatbot</div>
+      <div className="header">Atendimento</div>
       <div className="messages">
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`message ${message.type === 'user' ? 'user' : 'bot'}`}
+            className={`message ${message.type === 'user' ? 'userChat' : 'modelChat'}`}
           >
-            {message.text}
+            {message.type === 'user' ? (
+              <div className="icon-container userIconContainer">
+                <UserRound className='userIcon' />
+              </div>
+            ) : (
+              <div className="icon-container botIconContainer">
+                <Bot className='botIcon' />
+              </div>
+            )}
+            <div
+              className={`message-text ${message.type === 'user' ? 'userChat' : 'modelChat'}`}
+            >
+              <span>{message.text}</span>
+            </div>
           </div>
         ))}
+        <div ref={messagesEndRef}></div>
       </div>
       <div className="input-container">
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="Digite sua mensagem..."
           className="input"
         />
-        <button onClick={sendMessage} className="button">
+        <button onClick={sendMessage} className="neon-button">
           Enviar
         </button>
       </div>
